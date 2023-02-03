82103bca3a4d4df81f252bae158631b27879a208
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2023-02-03 10:23:45 +0200

    add new questions in built_in exercises

--- built_in/built_in.dart
+++ built_in/built_in.dart
@@ -1,4 +1,45 @@
 void main(List<String> args) {
+  //! EASY
+  // convert string to int
+  String number = '10';
+  int number2;
+  // output should be 10
+
+  // convert int to string
+  int number3 = 10;
+  String number4;
+  // output should be '10'
+
+  // uppercase and lowercase a string
+  String name = 'ahmed';
+  String upperCaseName;
+  String lowerCaseName;
+  // output should be AHMED and ahmed
+
+  // check if a string is empty or not
+  String name2 = '';
+  String name3 = 'ahmed';
+  bool isEmpty;
+  bool isNotEmpty;
+  // output should be true and false
+
+  // check if a string contains a substring
+  String name4 = 'ahmed';
+  String substring = 'med';
+  bool contains;
+  // output should be true
+
+  // check the type of a variable
+  dynamic name5 = 'ahmed';
+  int number5 = 10;
+  bool isString;
+  bool isInt;
+  // output should be true and false
+
+  //! MEDIUM
+
+  // convert map into Key- Value pairs in list without using for loop
+
   Map<String, int> grades = {'Ahmed': 75, 'Youssef': 82, 'Sherif': 90};
 
   // convert map into Key- Value pairs in list without using for loop
@@ -32,7 +73,13 @@ void main(List<String> args) {
 
   bool isAhmedExistInList;
 
-  
+  //! BONUS NULL SAFETY
+  // Search for this "??" double question mark in the code and try to understand what it does
+  // "https://jelenaaa.medium.com/what-are-in-dart-df1f11706dd6"
+  // check if the name is null or not if it is null then return "John" else return the name
+  String? namee;
+  String defaultName = 'John Doe';
+  String actualName;
 
-  // 
+  // check if the name is null or not if it is null then return "John" else return the name
 }
