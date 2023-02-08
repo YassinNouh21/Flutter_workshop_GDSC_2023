6cd0f74032880fc0c275df5a4ca3ed6e33f69c40
Ahmed Sherif <45398293+AhmedSherif4@users.noreply.github.com>
2023-02-08 02:38:17 +0200

    assignment for Future and Stream

---
+++ Future_Stream/future_stream.dart
@@ -0,0 +1,49 @@
+
+// Future
+
+//  1- create Function that output 'your name' after 5 secondes
+
+
+
+// 2- write this code by Future.value() using then - catchError - WhenComplete
+// // note: put x in value Like: Future.value(x)
+
+// int x = 2;
+// int y = 0;
+
+// try{
+// final divide = x~/y;
+// print(divide);
+// } catch {
+// (e)=>print(e);
+// } finally{
+// print("Done!");
+// }
+
+
+// 3- what the different between onError and catchError?
+// - write the answer as comment 
+
+/////////////////////
+
+// Stream
+
+// 1- write stream of integer using fromIterable (any 5 numbers)
+// and use fold to get the sum of all numbers
+// and use then to print it.
+
+
+// 2- write stream of integer using fromIterable (any 10 numbers)
+// -
+// after this code   final listen = yourStream.listen(
+//     (_) => _,
+//   );
+// -
+// use onData to print only event number 
+// then use onDone to output that it's DONE!, 
+// and use onError to handle any error would happen
+
+// // Hard
+// 3- Create Function that output your name characters by stream every 1 seconde, and use try catch to break it after finishing.
+
+ 
